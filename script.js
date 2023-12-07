a7be1145383329fb204f3c5a3182971eab561717
codebuilder-js <codebuilder.js@gmail.com>
2023-12-07 12:58:29 -0300

    Add some refactor to the original code

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const btnRock = document.querySelector('#btn-rock');
+const btnPaper = document.querySelector('#btn-paper');
+const btnScissors = document.querySelector('#btn-scissors');
+const result = document.querySelector('#result');
+
+btnRock.addEventListener('click', () => {
+  const playerChoice = btnRock.textContent.toLowerCase();
+  
+  rockPaperScissors(playerChoice, computerChoice());
+  
+  announceWinner();
+});
+
+btnPaper.addEventListener('click', () => {
+  const playerChoice = btnPaper.textContent.toLowerCase();
+  
+  rockPaperScissors(playerChoice, computerChoice());
+  
+  announceWinner();
+});
+
+btnScissors.addEventListener('click', () => {
+  const playerChoice = btnScissors.textContent.toLowerCase();
+  
+  rockPaperScissors(playerChoice, computerChoice());
+  
+  announceWinner();
+});
+
 let playerChoice;
 let playerScore = 0;
 let computerScore = 0;
@@ -10,53 +39,60 @@ function computerChoice() {
 }
 
 function rockPaperScissors(player, computer) {
-  playerChoice = prompt('Choose between rock, paper or scissors:').toLowerCase();
-  
-  console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+  result.textContent = `Player Score: ${playerScore} | Computer Score: ${computerScore}`;
   
   if(player === 'rock') {
     if(computer === 'scissors') {
-      console.log('player\'s rock beats scissors!');
+      result.innerHTML += '<br>player\'s rock beats scissors!';
       playerScore++;
     } else if (computer === 'paper') {
-      console.log('computer\'s paper beats rock!');
+      result.innerHTML += '<br>computer\'s paper beats rock!';
       computerScore++;
     } else {
-      console.log('it\'s a tie!')
+      result.innerHTML += '<br>it\'s a tie!';
     }
   }
   
   if(player === 'scissors') {
     if(computer === 'paper') {
-      console.log('player\'s scissors beats paper!');
+      result.innerHTML += '<br>player\'s scissors beats paper!';
       playerScore++;
     } else if (computer === 'rock') {
-      console.log('computer\'s rock beats scissors!');
+      result.innerHTML += '<br>computer\'s rock beats scissors!';
       computerScore++;
     } else {
-      console.log('it\'s a tie!')
+      result.innerHTML += '<br>it\'s a tie!';
     }
   }
   
   if(player === 'paper') {
     if(computer === 'rock') {
-      console.log('player\'s paper beats rock!');
+      result.innerHTML += '<br>player\'s paper beats rock!';
       playerScore++;
     } else if (computer === 'scissors') {
-      console.log('computer\'s scissors beats paper!');
+      result.innerHTML += '<br>computer\'s scissors beats paper!';
       computerScore++;
     } else {
-      console.log('it\'s a tie!')
+      result.innerHTML += '<br>it\'s a tie!';
     }
   }
 }
 
 function defineWinner() {
   if(playerScore > computerScore) {
-    console.log(`PLAYER IS VICTORY! FINAL SCORE - PLAYER: ${playerScore} | COMPUTER: ${computerScore}`);
+    result.textContent = `PLAYER IS VICTORY! FINAL SCORE - PLAYER: ${playerScore} | COMPUTER: ${computerScore}`;
   } else if ( playerScore < computerScore) {
-    console.log(`COMPUTER IS VICTORY! FINAL SCORE - PLAYER: ${playerScore} | COMPUTER: ${computerScore}`);
+    result.textContent = `COMPUTER IS VICTORY! FINAL SCORE - PLAYER: ${playerScore} | COMPUTER: ${computerScore}`;
   } else {
-    console.log(`THE TIE HAS BEEN PLANTED! FINAL SCORE - PLAYER: ${playerScore} | COMPUTER: ${computerScore}`);
+    result.textContent = `THE TIE HAS BEEN PLANTED! FINAL SCORE - PLAYER: ${playerScore} | COMPUTER: ${computerScore}`;
+  }
+}
+
+function announceWinner() {
+  if(playerScore === 5 || computerScore === 5) {
+    defineWinner();
+    
+    playerScore = 0;
+    computerScore = 0;
   }
 }
